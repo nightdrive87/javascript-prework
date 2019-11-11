67590e0ff5233f8204bb6ed70b5e66678052fcb3
Gosia <malgorzata.krysztofik@gmail.com>
2019-11-11 21:02:21 +0100

    Add conditional statements for playerMove

--- js/script.js
+++ js/script.js
@@ -11,6 +11,20 @@ if(randomNumber == 1){
 } else {
     computerMove = 'papier';
 }
-
-
 printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == 1){
+    playerMove = 'kamień';
+  } else if(playerInput == 2) {
+      playerMove = 'nozyce';
+  } else {
+      playerMove = 'papier';
+  }
+
+printMessage('Twój ruch to: ' + playerMove);
