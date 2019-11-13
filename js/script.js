9dda53dcb7b883e6d447f4e3a570bb2cf01b9fee
Gosia <malgorzata.krysztofik@gmail.com>
2019-11-13 21:46:36 +0100

    Modify by adding functions

--- js/script.js
+++ js/script.js
@@ -2,69 +2,78 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
+function getMoveName(argMoveId) {
+  if (argMoveId == 1) {
+    console.log('wybrano liczbe 1');
+    return 'kamień';
+  } else if (argMoveId == 2) {
+    return 'papier';
+  } else if (argMoveId == 3) {
+    return 'nozyce';
+  } else {
+    return 'nieznany ruch';
+  }
 
-if(randomNumber == 1){
-  computerMove = 'kamień';
-} else if(randomNumber == 2) {
-    computerMove = 'nozyce';
-} else {
-    computerMove = 'papier';
+  printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+  return 'nieznany ruch';
 }
+
+function displayResult(argComputerMove, argPlayerMove) {
+
+  console.log('moves:', argComputerMove, argPlayerMove);
+
+  if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
+    return 'Ty wygrywasz!';
+  }
+  else if (argComputerMove == 'kamień' && argPlayerMove == 'nozyce') {
+    return 'Ja wygrywam!';
+  }
+  else if (argComputerMove == 'kamień' && argPlayerMove == 'kamień') {
+    return 'Remis!';
+  }
+  else if (argComputerMove == 'kamień' && argPlayerMove == 'nieznany ruch') {
+    return 'Błąd gracza - wybierz liczbę 1, 2 lub 3';
+  }
+  else if (argComputerMove == 'papier' && argPlayerMove == 'kamień') {
+    return 'Ja wygrywam!';
+  }
+  else if (argComputerMove == 'papier' && argPlayerMove == 'nozyce') {
+    return 'Ty wygrywasz!';
+  }
+  else if (argComputerMove == 'papier' && argPlayerMove == 'papier') {
+    return 'Remis!';
+  }
+  else if (argComputerMove == 'papier' && argPlayerMove == 'nieznany ruch') {
+    return 'Błąd gracza - wybierz liczbę 1, 2 lub 3';
+  }
+  else if (argComputerMove == 'nozyce' && argPlayerMove == 'papier') {
+    return 'Ja wygrywam!';
+  }
+  else if (argComputerMove == 'nozyce' && argPlayerMove == 'kamień') {
+    return 'Ty wygrywsz!';
+  }
+  else if (argComputerMove == 'nozyce' && argPlayerMove == 'nozyce') {
+    return 'Remis!';
+  }
+  else if (argComputerMove == 'nozyce' && argPlayerMove == 'nieznany ruch') {
+    return 'Błąd gracza - wybierz liczbę 1, 2 lub 3';
+  }
+  else {
+    return 'Krytyczny błąd';
+  }
+}
+
+let computerMove = getMoveName(randomNumber);
+
 printMessage('Mój ruch to: ' + computerMove);
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
-
-if(playerInput == 1){
-    playerMove = 'kamień';
-  } else if(playerInput == 2) {
-      playerMove = 'nozyce';
-  } else if(playerInput == 3) {
-      playerMove = 'papier';
-  }
+let playerMove = getMoveName(playerInput);
 
 printMessage('Twój ruch to: ' + playerMove);
 
-if(computerMove == 'kamień' && playerMove == 'papier'){
-    printMessage('Ty wygrywasz!');
-  } 
-else if(computerMove == 'kamień' && playerMove == 'nozyce'){
-    printMessage('Ja wygrywam!');
-  }
-else if(computerMove == 'kamień' && playerMove == 'kamień'){
-    printMessage('Remis!');
-  }
-  else if(computerMove == 'kamień' && playerMove == 'nieznany ruch'){
-    printMessage('Błąd gracza - wybierz liczbę 1, 2 lub 3');
-  }
-  else if(computerMove == 'papier' && playerMove == 'kamień'){
-    printMessage('Ja wygrywam!');
-  }
-  else if(computerMove == 'papier' && playerMove == 'nozyce'){
-    printMessage('Ty wygrywasz!');
-  }
-  else if(computerMove == 'papier' && playerMove == 'papier'){
-    printMessage('Remis!');
-  }
-  else if(computerMove == 'papier' && playerMove == 'nieznany ruch'){
-    printMessage('Błąd gracza - wybierz liczbę 1, 2 lub 3');
-  }
-  else if(computerMove == 'nozyce' && playerMove == 'papier'){
-    printMessage('Ja wygrywam!'); 
-  }
-  else if(computerMove == 'nozyce' && playerMove == 'kamień'){
-    printMessage('Ty wygrywsz!'); 
-  }
-  else if(computerMove == 'nozyce' && playerMove == 'nozyce'){
-    printMessage('Remis!'); 
-  }
-  else if(computerMove == 'nozyce' && playerMove == 'nieznany ruch'){
-    printMessage('Błąd gracza - wybierz liczbę 1, 2 lub 3');
-  }
-  else {
-      printMessage('Krytyczny błąd');
-  }
+let result = displayResult(computerMove, playerMove);
+printMessage(result);
